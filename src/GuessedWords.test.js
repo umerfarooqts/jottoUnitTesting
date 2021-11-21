b21a6a0f8b4cbbe7674ade8f67337670f340339c
Umer Farooq <umer.farooq@tspirits.com>
2021-11-21 17:10:17 +0500

    Fixed some missing Unit tests

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,70 +1,114 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import {findByTestAttr, checkProps} from "../test/testUtil";
-import GuessedWords from "./GuessedWords";
+/* Functional Tests
+ * True testing of the behavior - Independent of shared state implementation
+ * The only change will be in the setup function
+ * It is important to know how to apply the initial state
+ * Initial State can contain props like success, secretWord, guessedWords
+ * Method for storing is different for Redux vs React Context
+ */
 
+import React from "react";
 
+// Since we are not going to do unit testing, we will need mount here.
+// This is because we don't want to run our test in Isolation
+// By using mount, we will get the state of the children as well.
+import { mount } from 'enzyme';
 
-const defaultProps = {
-    guessedWords: [{guessedWord: 'train', letterMatchCount: 3}]
-}
+import App from "./App";
+import { findByTestAttr } from "../test/testUtil";
+
+// The setup functions can be a bit different based on if we are using
+// Redux or React Context
 
 /**
- * Factory Function to create a ShallowWrapper for the GuessWord Component
- * @function setup
- * @param {object} props - Component props specific to this setup
- * @returns {ShallowWrapper}
+ *  Create a wrapper with specified initial conditions
+ *  then submit a guessed Word of 'train'
+ *  @function
+ *
+ *  @param {object} state - Initial Conditions
+ *  @returns {wrapper} - Enzyme Wrapper of mounted App Component
  */
-const setup = (props={}) => {
-    const setupProps = {...defaultProps, ...props};
-    return shallow(<GuessedWords {...setupProps}/>);
+const setup = (state = {}) => {
+    // TODO: Apply State
+    const wrapper = mount (<App />);
+
+    // Add a value to inputbox
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    inputBox.simulate('change', {target: { value: 'train' }});
+
+    // Simulate click on submit button
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {} });
+
+    return wrapper;
 }
 
-//Note: This is not working for some reasons.
-test('Does not throw any warnings with the expected props', ()=>{
-    checkProps(GuessedWords, defaultProps);
-})
-
-describe('If there are no words guessed', ()=>{
-
+describe.skip('No Words have been Guessed', ()=>{
     let wrapper;
     beforeEach(()=>{
-        wrapper = setup({guessedWords: []})
-    })
-    test('Renders without error', ()=>{
-        const component = findByTestAttr(wrapper, 'component-guessed-word');
-        expect(component.length).toBe(1);
-    })
+        wrapper = setup({
+            secretWord: 'party',
+            success: false,
+            guessWords: []
+        })
+    });
 
-    test('Renders instructions to guess a word', ()=>{
-        const instructions = findByTestAttr(wrapper, 'guess-instructions');
-        expect(instructions.text().length).not.toBe(0);
-    })
-})
+    test('Create a GuessWord table with one row', ()=>{
+        const GuessWordRows = findByTestAttr(wrapper, 'guessed-word');
+        expect(GuessWordRows).toHaveLength(1);
+    });
+});
 
-describe('If there are words guessed', ()=>{
-    const guessedWords = [
-        {guessedWord: 'agile', letterMatchCount: 1},
-        {guessedWord: 'train', letterMatchCount: 3},
-        {guessedWord: 'party', letterMatchCount: 5}
-        ]
-
+describe.skip('Some Words have been Guessed', ()=>{
     let wrapper;
     beforeEach(()=>{
-        wrapper = setup({guessedWords})
+        wrapper = setup({
+            secretWord: 'party',
+            success: false,
+            guessWords: [{guessedWord: 'agile', letterMatchCount: 1},
+                {guessedWord: 'train', letterMatchCount: 3}]
+        })
+    });
+
+    test('Create a GuessWord table with one row', ()=>{
+        const GuessWordRows = findByTestAttr(wrapper, 'guessed-word');
+        expect(GuessWordRows).toHaveLength(3);
+    });
+});
+
+describe.skip('We Guessed the secret word', ()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = setup({
+            secretWord: 'party',
+            success: false,
+            guessWords: [{guessedWord: 'agile', letterMatchCount: 1},
+                {guessedWord: 'train', letterMatchCount: 3}]
+        });
+
+        // Now we simulate a trigger to the right secretWord
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+        inputBox.simulate('change', {target: { value: 'party' }});
+        const submitButton = findByTestAttr(wrapper, 'submit-button');
+        submitButton.simulate('click', { preventDefault() {} });
+    });
+
+    test('There should be the right number of rows', ()=>{
+        const GuessWordRows = findByTestAttr(wrapper, 'guessed-word');
+        expect(GuessWordRows).toHaveLength(3);
+    });
+
+    test('Congrats message should be displayed', ()=>{
+        const congratsComponent = findByTestAttr(wrapper, 'component-congrats');
+        expect(congratsComponent).toHaveLength(1);
+        expect(congratsComponent.text().length).toBeGreaterThan(0);
+    });
+
+    test('Input Component Should be hidden', ()=>{
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+        const submitButton = findByTestAttr(wrapper, 'submit-button');
+        expect(inputBox.exists()).toBe(false);
+        expect(submitButton.exists()).toBe(false);
     });
 
 
-    test('Renders without error', ()=>{
-        const component = findByTestAttr(wrapper, 'component-guessed-word');
-        expect(component.length).toBe(1);
-    })
-    test('Renders guessed words section', ()=>{
-        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
-        expect(guessedWordsNode.length).toBe(1);
-    })
-    test('Correct number of guessed words', ()=>{
-        const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
-        expect(guessedWordsNodes.length).toBe(guessedWords.length);
-    })
 })
